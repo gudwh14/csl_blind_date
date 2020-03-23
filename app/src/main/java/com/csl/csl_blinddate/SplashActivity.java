d5fda720f995e63fb260e60858c88e5f3533dc01
차형조 <alxious@naver.com>
2020-03-24 00:25:26 +0900

    first commit

---
+++ app/src/main/java/com/csl/csl_blinddate/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.csl.csl_blinddate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        try {
+            final Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            /*Thread.sleep(1000);
+            startActivity(intent);
+            finish();*/
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
